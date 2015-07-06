0e501205227658d1762419df0f1887c105661b7b
JC <mail@jc.pe>
2015-07-05 23:56:51 -0500

    Hello World

---
+++ Framework/app/src/main/java/pe/jc/framework/MainActivity.java
@@ -0,0 +1,71 @@
+package pe.jc.framework;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText TxtNombres;
+    private EditText TxtApellidos;
+    private Button btnAceptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Obtenemos una referencia a los controles de la interfaz
+        TxtNombres = (EditText)findViewById(R.id.TxtNombres);
+        TxtApellidos = (EditText)findViewById(R.id.TxtApellidos);
+        btnAceptar = (Button)findViewById(R.id.BtnAceptar);
+
+        //Implementamos el evento click del boton
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el Intent
+                Intent intent =
+                        new Intent(MainActivity.this, SaludoActivity.class);
+
+                //Creamos la informacion a pasar entre actividades
+                Bundle b = new Bundle();
+                b.putString("NOMBRES", TxtNombres.getText().toString());
+                b.putString("APELLIDOS", TxtApellidos.getText().toString());
+
+                //Añadimos la información al intent
+                intent.putExtras(b);
+
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
